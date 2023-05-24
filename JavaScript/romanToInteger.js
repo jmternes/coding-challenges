5f586ca7de3dff65bc83dd25f2abbcfb1e2a6bbd
jmternes <68615887+jmternes@users.noreply.github.com>
2023-05-24 14:02:24 -0500

    roman numeral to integer js

---
+++ JavaScript/romanToInteger.js
@@ -0,0 +1,17 @@
+// Given a roman numeral, convert it to an integer.
+
+// Map roman numeral symbols to values, then iterate through each character in the input string. Add roman numeral logic, such as: If the current character is less than the next character, subtract the current character from the next character and add that value to the result. Otherwise, add the value of the current character to the result. Return the result.
+
+var romanToInt = function(s) {
+    var result = 0;
+    var roman = {I:1, V:5, X:10, L:50, C:100, D:500, M:1000}; // map roman numeral symbols to values
+    for (var i = 0; i < s.length; i++) {
+        if (roman[s[i]] < roman[s[i+1]]) {
+            result += roman[s[i+1]] - roman[s[i]];
+            i++;
+        } else {
+            result += roman[s[i]];
+        }
+    }
+    return result;
+};
